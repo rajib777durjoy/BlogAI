898a97fca5ce88cecf56a46110921a249b989073
rajib777durjoy <rajib777chanda@gmail.com>
2025-06-11 03:23:13 +0600

    gemini AI for dynamic  text generate done

--- src/app/BlogsPage/page.jsx
+++ src/app/BlogsPage/page.jsx
@@ -8,6 +8,9 @@ import { useAuth } from '@clerk/nextjs';
 import axiosPublic from '../AxiosPublic/page';
 import useAxios from '../useAxios/page';
 import Swal from 'sweetalert2';
+import { RiAiGenerate2 } from "react-icons/ri";
+import { RiGeminiFill } from "react-icons/ri";
+import { useState } from 'react';
 
 
 
@@ -16,10 +19,14 @@ import Swal from 'sweetalert2';
 
 const Image_API_KEY = `https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_ImgBB_API_KEY}`
 const BlogsPage = () => {
-    const user = useAuth()
-    console.log("user:", user?.userId)
-    const router = useRouter()
-    const Axiosinstance = useAxios()
+    const user = useAuth();
+    // console.log("user:", user?.userId)
+    const router = useRouter();
+    const Axiosinstance = useAxios();
+    const [AI_Text, setAI_Text] = useState('');
+    const [Text, SetText] = useState('');
+    const [TextTitle, setTextTitle] = useState('');
+    const [Description, setDescription] = useState('')
     const {
         register,
         handleSubmit,
@@ -27,19 +34,19 @@ const BlogsPage = () => {
         formState: { errors },
     } = useForm()
     const onSubmit = async (data) => {
-
+        // console.log('onsubmit for form hook')
         const ImageFile = { image: data?.Image[0] }
 
         //  if(!ImageFile){
         //     return ;
         //  }
-        console.log('data', data?.Image[0])
+        // console.log('data', data?.Image[0])
         const res = await axios.post(Image_API_KEY, ImageFile, {
             headers: {
                 'content-type': 'multipart/form-data'
             }
         })
-        console.log('image response', res.data.data.display_url)
+        // console.log('image response', res.data.data.display_url)
         const information = {
             title: data?.title,
             description: data?.description,
@@ -48,9 +55,9 @@ const BlogsPage = () => {
             like: 0,
             comment: 0,
         }
-        console.log("information", information)
+        // console.log("information", information)
         const response = await Axiosinstance.post(`/blogPost/${user?.userId}`, information);
-        console.log('response:', response)
+        // console.log('response:', response)
         if (response.data.data.insertedId > 0) {
 
             Swal.fire({
@@ -62,60 +69,92 @@ const BlogsPage = () => {
             });
         }
     }
+    const handlesubmitText = async (e) => {
+        e.preventDefault();
+        setAI_Text('')
+        console.log('handlesubmitText call:')
+        const text = e.target.GeminiText.value;
+        const res = await Axiosinstance.get(`/Gemini/textGenarate?text=${text}`,{})
+        // console.log('gemini response:', res.data.result)
+        if (res?.data?.result) {
+            setAI_Text(res.data.result)
+            document.getElementById('my_modal_5').close()
+        }
 
+    }
     return (
         <>
             <div className='w-[100%]'>
-                <div className='w-[100%]  flex justify-between my-5'>
-                    <div>
-                        <input type="text" placeholder="Search" className="input input-bordered  md:w-[800px]" />
+                {/* First form Start */}
+                <form onSubmit={handleSubmit(onSubmit)} className="bg-base-300 p-10 rounded-xl shadow-md w-[70%] mx-auto mt-4">
+                    <div className="flex flex-col-reverse gap-3 min-h-[300px]">
+                        <textarea
+                            className="flex-1 resize-none border-2 focus:outline-none p-2 rounded-md bg-base-300 "
+                            placeholder="What's on your mind?"
+                            rows={10}
+                            defaultValue={AI_Text}
+                            {...register("description", {
+                                onChange: (e) => {
+                                    setDescription(e.target.value);
+                                    console.log("description:", e.target.value);
+                                }
+                            })}
+                        ></textarea>
+                        <div className='flex w-[100%] items-center gap-4'>
+                            <input
+                                {...register("title", {
+                                    onChange: (e) => {
+                                        setTextTitle(e.target.value)
+                                        console.log('title:', e.target.value)
+                                    }
+                                })} 
+                                className='w-[95%] h-[40px] px-2 ' type='text' placeholder='Enter the title' /><button onClick={() => document.getElementById('my_modal_5').showModal()} type='button' className='btn rounded-full'>AI<RiGeminiFill className='text-2xl' /></button>
+                        </div>
+
                     </div>
-                    <button onClick={() => {
-                        document.getElementById('my_modal_5').showModal()
-                    }} className='btn bg-blue-500'>Create Post</button>
-                </div>
+                    <div className="flex justify-between mt-2">
+                        <input {...register("Image")} type="file" className="file-input" />
+                        <button className='btn flex items-center'>Generate Image <RiAiGenerate2 className='text-lg' /></button>
+                    </div>
+                    <div className='flex justify-between py-4'>
+                        <button
+                            type='submit'
+                            className="bg-blue-500 text-white px-4 py-1 rounded disabled:opacity-50" onClick={() => {
+                                document.getElementById('my_modal_5').close()
+                            }}  >
+                            Cancel
+                        </button>
+                        <button
 
-                <div>
-                    helojdfosfs
-                </div>
-                {/* Open the modal using document.getElementById('ID').showModal() method */}
-                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                    <form onSubmit={handleSubmit(onSubmit)} className="bg-base-300 p-4 rounded-xl shadow-md w-[700px] mx-auto mt-4">
-                        <div className="flex flex-col-reverse gap-3 min-h-[300px]">
-                            <textarea
-                                className="flex-1 resize-none border-2 focus:outline-none p-2 rounded-md bg-base-300 "
-                                placeholder="What's on your mind?"
-                                rows={10}
-                                {...register("description")}
-                            ></textarea>
-                            <input {...register("title")} className='w-[100%] h-[50px] border-2 px-2' type='text' placeholder='Enter the title'></input>
-                        </div>
-                        <div className="flex justify-between mt-2">
-                            <input {...register("Image")} type="file" className="file-input" />
-
-                        </div>
-                        <div className='flex justify-between py-4'>
-                            <button
-                                type='submit'
-                                className="bg-blue-500 text-white px-4 py-1 rounded disabled:opacity-50" onClick={() => {
-                                    document.getElementById('my_modal_5').close()
-                                }}  >
-                                Cancel
-                            </button>
-                            <button
-                                type='submit'
-                                className="bg-blue-500 text-white px-4 py-1 rounded disabled:opacity-50"       >
-                                Post
-                            </button>
-                        </div>
-                    </form>
-                </dialog>
+                            type='submit'
+                            disabled={(!TextTitle && !Description) || (!AI_Text && !TextTitle)}
+                            className="bg-blue-500 text-white px-4 py-1 rounded disabled:opacity-50"       >
+                            Post
+                        </button>
+                    </div>
+                </form>
+                {/* First form Close */}
             </div>
+            {/* -----------------------------------------------------------------------------------*/}
 
+            {/* --------------------Second Form Start------------------- */}
+            <dialog id="my_modal_5" className="modal  modal-bottom sm:modal-middle">
+                <form onSubmit={handlesubmitText} method="dialog" className='w-[600px] p-2  bg-base-300 rounded-lg'>
+                    <textarea onChange={(e) => {
+                        console.log('onchange methon', e.target.value)
+                        SetText(e.target.value)
+                    }} rows={5} className='w-[100%] border-0 p-2 ' name="GeminiText" placeholder='Ask Anythink' id=""></textarea>
+                    <div className='flex justify-between items-center'>
+                        <div onClick={() => {
+                            document.getElementById('my_modal_5').close()
+                        }} className="px-5 py-2 rounded-md bg-slate-300 font-bold">Close</div>
+                        <button disabled={!Text} type='submit' className='btn bg-blue-500 text-white font-bold'>Generate</button>
+                    </div>
+                </form>
+            </dialog>
+            {/* --------------------Second Form Close------------------- */}
         </>
-
-
-    );
+           );
 };
 
 export default BlogsPage;
--- src/app/component/AllBlog/page.jsx
+++ src/app/component/AllBlog/page.jsx
@@ -1,10 +1,19 @@
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Allblog = () => {
+    const router = useRouter()
     return (
-        <div>
-            
-        </div>
+     
+            <div className='w-[100%] flex justify-between mt-5'>
+                <div>
+                    <input type="text" placeholder="Search" className="input input-bordered border-2  md:w-[800px]" />
+                </div>
+                <button className='btn text-white font-medium bg-blue-400' onClick={() => {
+                    router.push('/BlogsPage')
+                }}>Create Post</button>
+            </div>
+
     );
 };
 
